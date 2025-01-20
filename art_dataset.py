4c095817359124d966c7d1ed0246f916d8c11254
tanha <72491850+tanhata@users.noreply.github.com>
2025-01-20 17:28:15 -0500

    Create art_dataset.py

---
+++ art_dataset.py
@@ -0,0 +1,19 @@
+from torch.utils.data import Dataset
+from PIL import Image
+import torchvision.transforms as transforms
+
+class ArtDataset(Dataset):
+    def __init__(self, image_paths, labels, transform=None):
+        self.image_paths = image_paths
+        self.labels = labels
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.image_paths)
+
+    def __getitem__(self, idx):
+        image = Image.open(self.image_paths[idx]).convert("RGB")
+        label = self.labels[idx]
+        if self.transform:
+            image = self.transform(image)
+        return image, label
